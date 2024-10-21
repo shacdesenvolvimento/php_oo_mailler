2b582478a29e887b0160bf61d254dfd18658122e
Saulo Henrique Araujo Carneiro <shacdesenvolvimento@gmail.com>
2024-10-21 09:19:57 -0300

    finalizado

--- processa_envio.php
+++ processa_envio.php
@@ -16,6 +16,7 @@
 		private $assunto = null;
 		private $mensagem = null;
 		private $retorno=null;
+		private $status=null;
 
 		public function __get($atributo) {
 			return $this->$atributo;
@@ -53,8 +54,8 @@
 			$mail->isSMTP();                                            //Send using SMTP
 			$mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
 			$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-			$mail->Username   = 'webcompleto2@gmail.com';                     //SMTP username
-			$mail->Password   = '!@#$4321';                               //SMTP password
+			$mail->Username   = 'infoshac@gmail.com';                     //SMTP username
+			$mail->Password   = 'pijv lrxl osvr wyf';                               //SMTP password
 			$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;       //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 			$mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
@@ -77,11 +78,12 @@
 
 			$mail->send();
 			$mensagem->__set('retorno','Menssagem enviada com Sucesso!!');
+			$mensagem->__set('status',1);
 	} catch (Exception $e) {
 		/* echo "Não foi possível enviar este e-mail! Por favor tente novamente mais tarde.";
 		echo 'Detalhes do erro: ' . $mail->ErrorInfo; */
-
-		$mensagem->__set('retorno','Menssagem enviada com Sucesso!!');
+		$mensagem->__set('status',2);
+		$mensagem->__set('retorno','Mensagem com erro'.$mail->ErrorInfo);
 	}
 ?>
 	<!DOCTYPE html>
@@ -93,17 +95,28 @@
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	</head>
 	<body>
-		<div class="container">
-			<div class="row">
-				<div class="col">
+		<div class="container mt-5">
+			<div class="row justify-content-md-center">
+				<div class="col-md-auto">
+					<?php if ($mensagem->__get("status")==1) {						
+					?>
 					<div class="alert alert-success" role="alert">
-						Um simples alerta success com. Clique nele, se quiser.
+						<h2>Mesangem enviada com sucesso</h2>
+					</div>
+					<?php } ?>
+					<?php if ($mensagem->__get("status")==2) {						
+					?>
+					<div class="alert alert-danger" role="alert">
+						<h2>Erro inesperado</h2>
+					</div>
+					<?php  }?>
+				</div>				
+			</div>
+				<div class="row ">
+					<div class="col-md-auto text-end">
+						<a href="index.php" class="btn btn-primary mt-3">Voltar</a>
 					</div>
 				</div>
-				<div class="row">
-				<a href="index.php" class="btn btn-primary">Voltar</a>
-				</div>
-			</div>
 		</div>
 	</body>
 	</html>
